0fa21f083609f9370a41b2d32531fb3c8dcc399b
Srinivas Edireswarapu <Srinivas@xmos.com>
2012-08-30 20:42:42 +0530

    app_slicekit_com_demo: CR LF changes to help screen

--- app_slicekit_com_demo/src/util.c
+++ app_slicekit_com_demo/src/util.c
@@ -2,21 +2,21 @@
 #include "common.h"
 
 char console_messages[NUM_CONSOLE_MSGS][CONSOLE_MSGS_MAX_MEN] = {
-		"Choose one of the following options \n e - echo data \n r - UART reconfiguration \n g - get file \n p - put file \n h - usage help\n",			//IDX_USAGE_HELP
-		"Welcome to MUART Slice Card demo\n Choose one of the following options \n e - echo data \n r - UART reconfiguration \n g - get file \n p - put file \n h - usage help\n",  //IDX_WELCOME_USAGE
-		"UART now echoes back the data entered\n", 				//IDX_ECHO_MODE_MSG
-		"Enter new baud rate for UART \n", 						//IDX_RECONF_MODE_MSG
-		"Restart serial console with new baud rate setting \n", //IDX_RECONF_SUCCESS_MSG
-		"Specify a valid baud rate \n", 						//IDX_RECONF_FAIL_MSG
-		"CRC mismatch for uploaded file, try again\n",			//IDX_CRC_FOR_GET_FILE_MSG
-		"CRC not found, try again\n",							//IDX_CRC_NA_FOR_GET_FILE_MSG
-		"Press any key to fetch file from UART \n", 			//IDX_PUT_FILE_MSG
-		"Buffered file data is lost \n", 						//IDX_FILE_DATA_LOST_MSG
-		"Invalid request\n Use 'get' option before using 'put' option \n",	//IDX_INVALID_PUT_REQUEST
-		"\nFile Received Vs File Transferred Timing (in millisec): \n",		//IDX_FILE_STATS
-		"UART is in Command mode. Press 'h' for help\n",		//IDX_CMD_MODE_MSG
-		"UART is in Data mode\n",								//IDX_DATA_MODE_MSG
-		"Invalid choice. Press 'h' for help\n",					//IDX_INVALID_USAGE
+		"Choose one of the following options \r\n e - echo data \r\n r - UART reconfiguration \r\n g - get file \r\n p - put file \r\n h - usage help\r\n",			//IDX_USAGE_HELP
+		"Welcome to MUART Slice Card demo\r\n Choose one of the following options \r\n e - echo data \r\n r - UART reconfiguration \r\n g - get file \r\n p - put file \r\n h - usage help\r\n",  //IDX_WELCOME_USAGE
+		"UART now echoes back the data entered\r\n", 				//IDX_ECHO_MODE_MSG
+		"Enter new baud rate for UART \r\n", 						//IDX_RECONF_MODE_MSG
+		"Restart serial console with new baud rate setting \r\n", //IDX_RECONF_SUCCESS_MSG
+		"Specify a valid baud rate \r\n", 						//IDX_RECONF_FAIL_MSG
+		"CRC mismatch for uploaded file, try again\r\n",			//IDX_CRC_FOR_GET_FILE_MSG
+		"CRC not found, try again\r\n",							//IDX_CRC_NA_FOR_GET_FILE_MSG
+		"Press any key to fetch file from UART \r\n", 			//IDX_PUT_FILE_MSG
+		"Buffered file data is lost \r\n", 						//IDX_FILE_DATA_LOST_MSG
+		"Invalid request\r\n Use 'get' option before using 'put' option \r\n",	//IDX_INVALID_PUT_REQUEST
+		"\r\nFile Received Vs File Transferred Timing (in millisec): \r\n",		//IDX_FILE_STATS
+		"UART is in Command mode. Press 'h' for help\r\n",		//IDX_CMD_MODE_MSG
+		"UART is in Data mode\r\n",								//IDX_DATA_MODE_MSG
+		"Invalid choice. Press 'h' for help\r\n",					//IDX_INVALID_USAGE
 };
 
 
